f928df2e97d13d4e44081c55d70109689f3e000c
Diego Gómez <da.gomezp@uniandes.edu.co>
2019-03-15 09:30:34 -0500

    Implementaación inicial Clase Hotel

---
+++ st-parranderos-jdo-est/src/main/java/uniandes/isis2304/parranderos/negocio/Hotel.java
@@ -0,0 +1,57 @@
+package uniandes.isis2304.parranderos.negocio;
+
+
+/**
+ * @author Diego Gómez
+ * TODO Contructor vacío y atributos de tablas
+ */
+public class Hotel implements VOHotel {
+
+	private String nombre;
+	
+
+	private String ciudad;
+	
+	private Integer numeroHabitaciones;
+	
+	
+	public Hotel(String nombre, String ciudad, Integer numeroHabitaciones) {
+		super();
+		this.nombre = nombre;
+		this.ciudad = ciudad;
+		this.numeroHabitaciones = numeroHabitaciones;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public void setNumeroHabitaciones(Integer numeroHabitaciones) {
+		this.numeroHabitaciones = numeroHabitaciones;
+	}
+	
+	@Override
+	public String getNombre() {
+		// TODO Auto-generated method stub
+		return nombre;
+	}
+
+	@Override
+	public String getCiudad() {
+		// TODO Auto-generated method stub
+		return ciudad;
+	}
+
+	@Override
+	public Integer getNumeroHabitaciones() {
+		// TODO Auto-generated method stub
+		return numeroHabitaciones;
+	}
+	
+	
+
+}
---
+++ st-parranderos-jdo-est/src/main/java/uniandes/isis2304/parranderos/negocio/VOHotel.java
@@ -0,0 +1,11 @@
+package uniandes.isis2304.parranderos.negocio;
+
+public interface VOHotel {
+	
+	public String getNombre();
+	
+	public String getCiudad();
+	
+	public Integer getNumeroHabitaciones();
+
+}
